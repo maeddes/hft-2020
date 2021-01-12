858e55c121efcf42a2d8ce428c3d4c7c50f9e324
Matthias Haeussler <maeddes@gmail.com>
2021-01-12 07:45:41 +0100

    clean

--- code/hello-world-new/src/main/java/de/hftstuttgart/helloworldnew/CustomerRepository.java
+++ code/hello-world-new/src/main/java/de/hftstuttgart/helloworldnew/CustomerRepository.java
@@ -8,6 +8,6 @@ public interface CustomerRepository extends CrudRepository<Customer, Long> {
 
     List<Customer> findByLastName(String lastName);
     List<Customer> findByFirstName(String firstName);
-
     Customer findById(long id);
+    
 }
